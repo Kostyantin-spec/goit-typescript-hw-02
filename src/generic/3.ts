99d38b0c1382a8c2d42b0b6247cab02821f32193
Kostyantin-spec <134406433+Kostyantin-spec@users.noreply.github.com>
2024-02-19 08:46:20 +0200

    and

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,8 +3,8 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge (objA, objB) {
+function merge<T extends Object, U>(objA: T, objB: U): Object {
   return Object.assign(objA, objB);
 }
 
-export {}
+export { };
